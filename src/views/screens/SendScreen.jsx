dcd6e4e31a2b8c07b0198c243ca716bc2fc45572
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2019-02-12 01:33:36 +0300

    input logic updated

--- src/views/screens/SendScreen.jsx
+++ src/views/screens/SendScreen.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { View, TouchableOpacity, Text, TextInput } from 'react-native';
+import { ScrollView, View, TouchableOpacity, Text, TextInput } from 'react-native';
 import { generateClaimLink } from './../../actions/wallet';
 import { formatAmount } from '../../utils/helpers';
 import styles from './styles';
@@ -23,13 +23,13 @@ class SendScreen extends React.Component {
 	    return;
 	}	
 	
-	if (this.props.balance < this.state.amount) {
+	if (this.props.balance < this.state.amount / 100) {
 	    alert(`Amount should be less than balance ($${formatAmount(this.props.balance)})`);
 	    return;
 	}
 	try { 
 	    this.props.generateClaimLink({
-		amount: this.state.amount * 100,
+		amount: this.state.amount,
 		navigator: this.props.navigator
 	    });
 	} catch(err) {
@@ -37,42 +37,51 @@ class SendScreen extends React.Component {
 	    alert("Error");
 	}
     }
+
+    _handleInput(val) {
+	console.log({newVal: val, oldVal: this.state.amount});
+	
+	// we don't want users to have , input
+	if (val.indexOf(',') >= 0) {
+	    return;
+	}
+
+	// we don't want users to have . input	
+	// const hasSecondDot = val.substr(val.indexOf('.')+1).indexOf('.') >= 0;
+	// if (hasSecondDot) {
+	if (val.indexOf('.') >= 0) {	
+	    return;
+	}
+
+
+	
+	this.setState({ amount: val});	
+    }
     
     render() {
         return (
-            <View style={styles.screenContainer}>
+            <ScrollView
+	       keyboardDismissMode="on-drag"
+	       keyboardShouldPersistTaps='always' >
+	      <View style={styles.screenContainer}>
                 <View style={styles.sendScreenContainer}>
                     <View>
                         <Text style={styles.title}>Send Screen</Text>
                     </View>
                     <View style={{...styles.sendInputContainer, marginTop:50}}>
-                        <Text style={styles.sendScreenText}>You're sending $</Text>
+		      <TouchableOpacity onPress={() => this.textInputRef.focus()}>
+			<Text style={styles.sendScreenText}>You're sending ${this.state.amount && Number(this.state.amount / 100).toFixed(2)}</Text>
+			</TouchableOpacity>
                         <TextInput
                             keyboardType='numeric'
-                            autoFocus={true}
-                            style={{ ...styles.sendScreenText, width: 100 }}
-                           onChangeText={(val) => {
-			       let amount;
-			       console.log({val});
-			       if (val.length < 4) {
-				    amount = val / 100;
-				} else {
-				   const digits = val.substring(val.indexOf('.') + 1).length;
-						    if (digits === 3) {
-							amount = val * 10;
-						    } else {							
-							amount = val;
-						    }
- 						    }
-						    if (!amount || amount === NaN || amount === "NaN") {
-							amount = 0;
-						    }
-						    console.log({amount});
-						    this.setState({ amount: String(Number(amount).toFixed(2)) });
-						}}
+                           autoFocus={true}
+			   caretHidden={true}
+			   ref={ref => this.textInputRef = ref}
+                           style={{width: 0, color: "#fff" }}
+                            onChangeText={this._handleInput.bind(this)}
 			    value={this.state.amount}
                             underlineColorAndroid='black'
-                        />
+                           />
                     </View>
                     <View style={{ alignItems: 'center' }}>
                       <TouchableOpacity style={styles.buttonContainer} onPress={this.onSend.bind(this)}>
@@ -82,7 +91,8 @@ class SendScreen extends React.Component {
                 </View>
                 <View style={{ flex: 1 }}>
                 </View>
-            </View>
+		</View>
+            </ScrollView>
         );
     }
 }
